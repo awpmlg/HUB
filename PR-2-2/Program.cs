6388c1e5caf6be585961b4e5e264a678876df4a0
awpmlg <awp_mlg@list.ru>
2020-09-04 15:17:11 +0300

    PR-2-2

---
+++ PR-2-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PR_2_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+      string[] sa = s.Split(" ");
+
+      Array.Sort(sa);
+
+      s = string.Join(" ", sa); Console.WriteLine(s);
+    }
+  }
+}
